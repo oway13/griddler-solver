ccd795f304fede3e7ae95ffa7e9fc200edec88ee
Oway13 <wyattcody133@gmail.com>
2019-03-15 12:47:07 -0500

    Wrote header, plan for function that will do bulk of algorithm

--- solver.py
+++ solver.py
@@ -10,3 +10,13 @@ def common(bitlist):
     for b in bitlist:
         comlist &= b
     return comlist
+
+
+#Inputs: list of 0/1 integers, ordered list for order and length of runs
+#Process: generates list of all possible bit strings that match requirements,
+#         that also matches bits that are already there. Then calls common to
+#         find the bits that are common to all bit strings. These are ones we
+#         know are correct.
+#Returns: list of 0/1 integers, hopefully partially solved, but not all will be
+def partial_solve_array(intlist, intreqs):
+    return
